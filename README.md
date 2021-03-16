16758c8e8252a8b30aeca43a616357aac1180c19
Itzbenz <gamerrindoubuntu@gmail.com>
2021-03-16 03:49:29 +0100

    oh no

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Mindustry-Mods-Template
-V6
+V6\
+Note: automatically use d8 and make dexed jar if detected
+
+### How to get indexed by mods browser
+![](https://cdn.discordapp.com/attachments/713346278003572777/821210982449807380/unknown.png)
+
+### How to make jar for android
+- install Android SDK ???
+- add to ANDROID_HOME environment
+- gradle makeJar
+- Dex = Dex only no class
+- Example-Mods = class only no dex
+- Example-Mods-Dexed = class and dex
+
+![](https://cdn.discordapp.com/attachments/713346278003572777/821212560820404274/unknown.png)
---
+++ src/main/java/Premain/Main.java
@@ -0,0 +1,38 @@
+package Premain;
+
+import arc.Core;
+import arc.Events;
+import arc.scene.Action;
+import arc.scene.Scene;
+import arc.scene.ui.Dialog;
+import arc.util.Log;
+import mindustry.Vars;
+import mindustry.game.EventType;
+import mindustry.gen.Icon;
+import mindustry.mod.Mod;
+import mindustry.ui.fragments.Fragment;
+
+public class Main extends Mod {
+	boolean stat;
+	@Override
+	public void init() {
+		//do you like lambda ?
+		Core.settings.getBoolOnce("Test", ()->{
+			Events.on(EventType.ClientLoadEvent.class, s ->{
+				new Dialog("Test"){{
+					cont.button("Yeet", Icon.image, ()->{
+						Vars.ui.showInfoFade("Yeet");
+					}).growX();
+					cont.row();
+					cont.button("Close", Icon.image, this::hide).growX();
+				}}.show();
+			});
+		});
+		Vars.ui.settings.main.table(t ->{
+			t.check("Boolean", s->{
+				stat = s;
+				Log.info(s);
+			});
+		}).growX();
+	}
+}
